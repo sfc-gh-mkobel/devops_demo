490ac56af5e6eb44bf6b3d9b65b82a5671444448
Meny Kobel <meny.kobel@snowflake.com>
2025-01-15 15:18:10 +0200

    remove toml import

--- devops/account_level/warehouse.py
+++ devops/account_level/warehouse.py
@@ -2,15 +2,7 @@ from snowflake.core import Root
 from snowflake.snowpark import Session
 from snowflake.core.warehouse import Warehouse
 import os
-import toml
 
-
-# PRIVATE_KEY_FILE = os.getenv("SNOWFLAKE_PRIVATE_KEY_FILE")
-# connection_params= toml.load(".snowflake/config.toml")['connections']['DEVOPS']
-# connection_params['private_key_file']=PRIVATE_KEY_FILE
-# session = Session.builder.configs(connection_params).create()
-# session.sql("USE ROLE ACCOUNTADMIN")
-# root = Root(session)
 
 root = Root(Session.builder.getOrCreate())
 
